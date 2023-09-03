5b706c9bd0e5c600f109d84e27bc22e8ab334e24
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-03 08:42:54 +0100

    Add simple api

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const http = require("http");
+const fs = require("fs");
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const productData = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+      "my-header": "Hi-hi",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening on port 8000");
+});
