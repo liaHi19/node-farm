d299bfb6d93b36f61302afceab863ed2e80b979c
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-03 14:28:21 +0100

    Add getting overview data

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const dataObj = JSON.parse(data);
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+
+  return output;
+};
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+  //overview
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const cardsTemp = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const result = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsTemp);
+
+    res.end(result);
+
+    //product
+  } else if (pathname === "/product") {
+    res.end;
+  }
+
+  // api
+  else if (pathname === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(data);
+  } else {
+    // not found
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+      "my-header": "Hi-hi",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening on port 8000");
+});
